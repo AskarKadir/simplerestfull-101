9444cfbf1997ff67f32aff95c93dac6e6ecb2aae
Askar Kadir <askarkadir07@gmail.com>
2022-12-04 22:24:02 +0700

    membuat project

---
+++ src/main/java/com/askar/pws/askar101/Askar101Application.java
@@ -0,0 +1,13 @@
+package com.askar.pws.askar101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Askar101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Askar101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/askar/pws/askar101/Askar101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askar.pws.askar101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Askar101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
