aaf41fe35d56cef47a29db8a8bc4a4bfa4619d86
Askar Kadir <askarkadir07@gmail.com>
2022-12-15 10:02:16 +0700

    menambahkan komen pada Product java

--- src/main/java/com/askar/pws/askar101/product/Product.java
+++ src/main/java/com/askar/pws/askar101/product/Product.java
@@ -14,34 +14,43 @@ public class Product {
     private String quantity;
     private String price;
 
+    
+    //untuk mengambil data dari varibel quantity
     public String getQuantity() {
         return quantity;
     }
 
+    //untuk mengisi atau mengubah data dari varibel quantity
     public void setQuantity(String quantity) {
         this.quantity = quantity;
     }
 
+    //untuk mengambil data dari varibel price
     public String getPrice() {
         return price;
     }
 
+    //untuk mengisi atau mengubah data dari varibel price
     public void setPrice(String price) {
         this.price = price;
     }
 
+    //untuk mengambil data dari varibel id
     public String getId() {
         return id;
     }
 
+    //untuk mengisi atau mengubah data dari varibel id
     public void setId(String id) {
         this.id = id;
     }
 
+    //untuk mengambil data dari variabel name
     public String getName() {
         return name;
     }
-
+    
+    //untuk mengisi atau mengubah data dari variabel name
     public void setName(String name) {
         this.name = name;
     }
